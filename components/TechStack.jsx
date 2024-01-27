84c9b891e0d7b0c03f69df4877871452288ce7cb
r2hu1 <newasram86@gmail.com>
2024-01-28 00:10:50 +0530

    []

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -14,35 +14,35 @@ export default function TechStack() {
                 <p className="text-xs -mt-1 max-w-xs mx-auto text-center">Here are some<span className="text-primary">*</span> of the technologies I use for building my projects.</p>
                 <div className="mt-6">
                     <ul className="grid grid-cols-1 md:grid-cols-2 gap-3 mb-5">
-                        <li className="p-3 hover:shadow-2xl cursor-pointer transition border rounded-md w-full md:w-auto">
+                        <li className="p-3 cursor-pointer transition border rounded-md w-full md:w-auto">
                             <div className="flex flex-col">
                                 <h1 className="text-lg"><IoLogoJavascript className="fill-yellow-400 w-8 h-8 hover:animate-pulse" />JavaScript</h1>
                                 <p className="text-xs -mt-1">A general-purpose, object-oriented scripting language.</p>
                             </div>
                         </li>
 
-                        <li className="p-3 hover:shadow-2xl cursor-pointer transition border rounded-md w-full md:w-auto">
+                        <li className="p-3 cursor-pointer transition border rounded-md w-full md:w-auto">
                             <div className="flex flex-col">
                                 <h1 className="text-lg"><SiTailwindcss className="fill-blue-400 w-8 h-8 hover:animate-pulse" />TailwindCSS</h1>
                                 <p className="text-xs -mt-1">A utility-first CSS framework for rapid UI development.</p>
                             </div>
                         </li>
 
-                        <li className="p-3 hover:shadow-2xl cursor-pointer transition border rounded-md w-full md:w-auto">
+                        <li className="p-3 cursor-pointer transition border rounded-md w-full md:w-auto">
                             <div className="flex flex-col">
                                 <h1 className="text-lg"><DiNodejs className="fill-green-400 w-8 h-8 hover:animate-pulse" />NodeJS</h1>
                                 <p className="text-xs -mt-1">A JavaScript runtime built on Chrome's V8 JavaScript engine.</p>
                             </div>
                         </li>
 
-                        <li className="p-3 hover:shadow-2xl cursor-pointer transition border rounded-md w-full md:w-auto">
+                        <li className="p-3 cursor-pointer transition border rounded-md w-full md:w-auto">
                             <div className="flex flex-col">
                                 <h1 className="text-lg"><TbBrandNextjs className="w-8 h-8 hover:animate-pulse" />NextJS</h1>
                                 <p className="text-xs -mt-1">A React framework for server-side rendering.</p>
                             </div>
                         </li>
 
-                        <li className="p-3 hover:shadow-2xl cursor-pointer transition border rounded-md w-full md:w-auto">
+                        <li className="p-3 cursor-pointer transition border rounded-md w-full md:w-auto">
                             <div className="flex flex-col">
                                 <h1 className="text-lg"><FaReact className="fill-blue-600 w-8 h-8 hover:animate-pulse" />ReactJS</h1>
                                 <p className="text-xs -mt-1">A JavaScript library for building user interfaces.</p>
