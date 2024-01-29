aee86588e9364e0a965e4fa983623c00c7d6a0b8
Rahul Rajput <87074472+r2hu1@users.noreply.github.com>
2024-01-29 22:23:17 +0530

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -5,6 +5,6 @@ export default function robots() {
         allow: '/',
         disallow: '/private/',
       },
-      sitemap: '/sitemap.xml',
+      sitemap: 'https://rahul.eu.org/sitemap.xml',
     }
   }
