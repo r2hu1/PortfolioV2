1be311316a7b7d47d5f2c782a54bc90dcabf0681
Rahul Rajput <87074472+r2hu1@users.noreply.github.com>
2024-01-29 22:27:41 +0530

    Update ProjectCard.jsx

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
     return(
         <div className="rounded-md border grid gap-3 p-3 w-fit bg-background">
             <div>
-                <Image src={props.image} width={340} height={340} className="rounded-md bg-secondary aspect-video h-full w-full max-w-sm md:max-w-[370px] lg:w-[365px]"/>
+                <Image src={props.image} alt={props.title} width={340} height={340} className="rounded-md bg-secondary aspect-video h-full w-full max-w-sm md:max-w-[370px] lg:w-[365px]"/>
             </div>
             <div>
                 <h1 className="text-base font-medium text-gray-900 dark:text-gray-50">{props.title}</h1>
