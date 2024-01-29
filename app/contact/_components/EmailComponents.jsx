eb476442bc5c88d575c58f03e27bd4ab3fcc9f58
r2hu1 <newasram86@gmail.com>
2024-01-29 13:57:15 +0530

    []

--- app/contact/_components/EmailComponents.jsx
+++ app/contact/_components/EmailComponents.jsx
@@ -3,7 +3,7 @@ import { Tailwind } from "@react-email/components";
 export default function EmailComponents({ name, email, message }) {
     return (
         <Tailwind>
-            <div className="flex px-6 flex-col gap-6 absolute inset-0 -z-10 min-h-[600px] w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:34px_34px] p-3">
+            <div className="grid gap-6 absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:34px_34px] p-3">
                 <div>
                     <h1 className="text-lg">{name}<span className="text-primary">.</span></h1>
                     <p className="text-xs -mt-1">{email}</p>
