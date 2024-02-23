6b2f2a9cebf7b19cb0aea0c8b601920f4b07a13f
Rahul Rajput <newasram86@gmail.com>
2024-02-23 14:38:25 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Page() {
       {/* Hero  */}
       <div className="grid place-items-center gap-6 py-20 -mt-5 mb-5">
         <div className="md:max-w-2xl max-w-xl px-5">
-          <h1 className="text-center text-3xl text-wrap font-semibold text-gray-900 dark:text-gray-50 sm:text-4xl">Hi<span className="text-primary">,</span> I am Rahul Rajput a full stack <span className="text-primary">Developer</span> <span className="animate-text-gradient inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400">from India<span className="text-primary">.</span></span></h1>
+          <h1 className="text-center text-3xl text-wrap font-bold text-gray-900 dark:text-gray-50 sm:text-4xl">Hi<span className="text-primary">,</span> I am Rahul Rajput a full stack <span className="text-primary">Developer</span> <span className="animate-text-gradient inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400">from India<span className="text-primary">.</span></span></h1>
           <p className="text-center text-sm mt-1 text-gray-600 dark:text-gray-200">An extensive display of my full-stack development skills, experiences, and projects, demonstrating my proficiency and commitment to coding<span className="text-primary">.</span></p>
         </div>
         <div className="flex items-center justify-center gap-2">
